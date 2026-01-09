1715813ee3c2c7dda4851d23e9a33f5a5259e02d
P01RAJASEKHY <p01rajasekhy@discovery.co.za>
2026-01-09 12:24:48 +0530

    manual ack at consumer side

--- src/main/java/com/raja/consumer/RabbitMQConsumer.java
+++ src/main/java/com/raja/consumer/RabbitMQConsumer.java
@@ -1,23 +1,39 @@
 package com.raja.consumer;
 
 
+import com.rabbitmq.client.Channel;
 import com.raja.dto.UserCreated;
 import com.raja.dto.UserUpdated;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @Slf4j
 public class RabbitMQConsumer {
 
-    @RabbitListener(queues = "${rabbitmq.queues.user-created.queue-name}")
-    public void consumeUserCreated(UserCreated userCreated) {
-        log.info("User Created Message: " + userCreated);
+    @RabbitListener(queues = "${rabbitmq.queues.user-created.queue-name}", ackMode = "MANUAL")
+    public void consumeUserCreated(UserCreated userCreated, Message message, Channel channel) throws Exception {
+
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        try {
+            System.out.println("Received message: " + userCreated);
+            processOrder(userCreated);
+            channel.basicAck(deliveryTag, false);
+        } catch (Exception ex) {
+            channel.basicNack(deliveryTag, false, false);
+            log.error("Error processing message: {}", ex.getMessage());
+            throw ex;
+        }
+    }
+    private void processOrder(UserCreated userCreated) {
+        log.info("User Created  Message:{}", userCreated);
     }
 
     @RabbitListener(queues = "${rabbitmq.queues.user-updated.queue-name}")
     public void consumeUserUpdated(UserUpdated userUpdated) {
-        log.info("User Updated Message:" + userUpdated);
+        log.info("User Updated Message:{}", userUpdated);
     }
 }
